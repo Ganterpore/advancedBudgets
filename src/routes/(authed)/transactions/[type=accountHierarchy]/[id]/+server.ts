e298ad4c41708aa6ba7bcf12011938f65c26fbad
Mitchell Hatfield <mitchell.hatfield@ue.com.au>
2024-05-15 18:50:22 +1000

    Refactored notifications endpoints

--- src/routes/(authed)/transactions/[type=accountHierarchy]/[id]/+server.ts
+++ src/routes/(authed)/transactions/[type=accountHierarchy]/[id]/+server.ts
@@ -1,34 +1,46 @@
-import type { RequestHandler } from "@sveltejs/kit";
+import type { RequestEvent, RequestHandler } from "@sveltejs/kit";
 import { json, error } from "@sveltejs/kit";
 import { newTransaction } from "../../transactionModel";
 import { AccountHierarchy } from "../../../../../params/accountHierarchy";
 import { TransactionType } from "../../types";
 import { assignTransaction } from "../../../account/savings/savingsController";
 
-export const POST: RequestHandler = async ({ request, cookies, params }) => {
+type Data = {
+  amount: number,
+  description: string,
+  type: TransactionType
+}
+
+async function validateFields ({ request, cookies, params }: RequestEvent<Partial<Record<string, string>>, string | null>) {
   const authToken = cookies.get('auth_token')
-  if (!authToken) return json({ status: 401 })
+  if (!authToken) throw error(401)
   const { type, id: accountId } = params
   const data = await request.json()
 
   if (!data.amount) return error(400,'Transaction value must not be empty')
-  if (!data.type || data.type === TransactionType.UNSELECTED) return error(400, 'Transaction type must not be empty')
+  if (!data.type || data.type === TransactionType.UNSELECTED) throw error(400, 'Transaction type must not be empty')
   if (!accountId) return error(400, 'Account ID cannot be empty')
-
   if (type === AccountHierarchy.PARENT_ACCOUNT) {
-    if (data.type !== TransactionType.GROUPED_SAVING) return error(400, `Cannot send transaction type ${data.type} to parent account`)
-    if (data.amount <= 0) return error(400, 'Grouped savings transactions cannot be negative')
-    const suggestions = await assignTransaction(data.amount, Number(accountId))
-    const promises = suggestions.map(s => newTransaction({
-      account: s.account,
-      amount: s.amount,
-      description: data.description
-    }))
-    const ids = await Promise.all(promises)
-    return json({ ids }, { status: 201 })
+    if (data.type !== TransactionType.GROUPED_SAVING) throw error(400, `Cannot send transaction type ${data.type} to parent account`)
+    if (data.amount <= 0) throw error(400, 'Grouped savings transactions cannot be negative')
+  } else {
+    if (data.type !== TransactionType.INDIVIDUAL) throw error(400, `Cannot send transaction type ${data.type} to account`)
   }
-  if (data.type !== TransactionType.INDIVIDUAL) return error(400, `Cannot send transaction type ${data.type} to account`)
+  return { accountId: Number(accountId), data }
+}
 
+async function handleGroupedSavingTransaction (accountId: number, data: Data) {
+  const suggestions = await assignTransaction(data.amount, Number(accountId))
+  const promises = suggestions.map(s => newTransaction({
+    account: s.account,
+    amount: s.amount,
+    description: data.description
+  }))
+  const ids = await Promise.all(promises)
+  return json({ ids }, { status: 201 })
+}
+
+async function handleIndividualTransaction (accountId: number, data: Data) {
   let id
   try {
     id = await newTransaction({...data, account: accountId})
@@ -37,4 +49,15 @@ export const POST: RequestHandler = async ({ request, cookies, params }) => {
     return error(500, 'Failed to upload transaction. Try again.')
   }
   return json({ id }, { status: 201 })
+}
+
+export const POST: RequestHandler = async (props) => {
+  const { accountId, data } = await validateFields(props)
+  switch (data.type) {
+    case TransactionType.GROUPED_SAVING:
+      return await handleGroupedSavingTransaction(accountId, data)
+    case TransactionType.INDIVIDUAL:
+      return handleIndividualTransaction(accountId, data)
+  }
+  throw error(400, `Cannot send transaction type ${data.type} to account`)
 }

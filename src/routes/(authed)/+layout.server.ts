1b2bd733ff8b6b1afd2df5129e4e62b1d3db5b41
Mitchell Hatfield <mitchell.hatfield@ue.com.au>
2024-06-13 19:03:36 +1000

    Attempt two at this fix

--- src/routes/(authed)/+layout.server.ts
+++ src/routes/(authed)/+layout.server.ts
@@ -4,14 +4,15 @@ import { getUserFromToken } from "../login/userModel";
 import { validateToken } from "$lib/utils";
 import { VERCEL_URL } from '$env/static/private'
 
-export const load: LayoutServerLoad = async ({ cookies, url }) => {
+export const load: LayoutServerLoad = async ({ cookies }) => {
   const authToken = cookies.get('auth_token')
   if (!authToken) {
-    throw redirect(303, `/login?redirectTo=${url.pathname}`)
+    throw redirect(303, '/login')
   }
   try {
     await validateToken(authToken)
   } catch (e) {
+    cookies.delete('auth_token', { path: '/' })
     throw redirect(303, '/login');
   }
   let user

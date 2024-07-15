5d485a8a4b5f47af2c2f0acd9c76b6841d9c31da
Mitchell Hatfield <mitchell.hatfield@ue.com.au>
2024-07-15 21:01:10 +1000

    Added ability to skip login in local

--- src/routes/(authed)/+layout.server.ts
+++ src/routes/(authed)/+layout.server.ts
@@ -1,7 +1,7 @@
 import type { LayoutServerLoad } from './$types'
 import { getUserById } from '$lib/models/userModel'
 export const load: LayoutServerLoad = async ({ locals }) => {
-  const user = await getUserById(Number(locals.user!.id))
+  const user = locals.user ? await getUserById(Number(locals.user.id)) : undefined
   return {
     user
   }

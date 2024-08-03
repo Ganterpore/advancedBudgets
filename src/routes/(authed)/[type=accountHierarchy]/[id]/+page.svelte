7b4d2efaa2eeba24afe7468fedde139b62d3c362
Mitchell Hatfield <mitchell.hatfield@ue.com.au>
2024-08-03 14:17:20 +1000

    Added total income and expenses for a month

--- src/routes/(authed)/[type=accountHierarchy]/[id]/+page.svelte
+++ src/routes/(authed)/[type=accountHierarchy]/[id]/+page.svelte
@@ -7,8 +7,28 @@
   import ParentAccountPopup from '$lib/components/accountComponents/ParentAccountPopup.svelte'
   import AccountPopup from '$lib/components/accountComponents/AccountPopup.svelte'
   import ArchiveAccountPopup from '$lib/components/accountComponents/ArchiveAccountPopup.svelte'
+  import type { TransactionWithParent } from '$lib/types/transactionTypes'
+  import TransactionMonthHeader from '$lib/components/transactionComponents/TransactionMonthHeader.svelte'
 
   export let data
+  const transactions: TransactionWithParent[] = data.transactions
+  const transactionsByMonth = transactions.reduce((monthMapping, transaction) => {
+    const year = transaction.transactionTime.getFullYear()
+    const month = transaction.transactionTime.toLocaleString('default', { month: 'long' });
+    const key = `${year}_${month}`
+    if (!monthMapping[key]){
+      monthMapping[key] = {
+        transactionList: [],
+        positiveSum: 0,
+        negativeSum: 0
+      }
+    }
+    monthMapping[key].transactionList.push(transaction)
+    if (transaction.amount > 0) monthMapping[key].positiveSum += transaction.amount
+    else monthMapping[key].negativeSum += transaction.amount
+    return monthMapping
+  }, {})
+  console.log(transactionsByMonth)
 
   async function editAccount () {
     if (data.isParent) {
@@ -32,13 +52,27 @@
 
 <div class="container">
   <div class="listContainer">
-    <List secondary list={data.transactions.map(t => ({id: t.id, header: TransactionListItem, headerProps: {
-        id: t.id,
-        description: t.description,
-        amount: t.amount,
-        accountName: t.accountName
+    <List list={Object.keys(transactionsByMonth).map(key => ({
+      id: key,
+      header: TransactionMonthHeader,
+      headerProps: {
+        dateString: key,
+        positiveSum: transactionsByMonth[key].positiveSum,
+        negativeSum: transactionsByMonth[key].negativeSum
+      },
+      child: List,
+      childProps: {
+        secondary: true,
+        list: transactionsByMonth[key].transactionList.map(t => ({
+          id: t.id, header: TransactionListItem, headerProps: {
+            id: t.id,
+            description: t.description,
+            amount: t.amount,
+            accountName: t.accountName
+          }
+        }))
       }
-    }))}/>
+    }))} />
   </div>
 </div>
 
